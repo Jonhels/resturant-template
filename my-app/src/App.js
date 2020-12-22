f592a7ce6031e92c10ef35162918ed339047a3d5
Jonhels <jonhels8888@gmail.com>
2020-12-22 08:34:12 +0100

    added simple navigation with style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
+import react from 'react';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Resturant</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
 
 export default App;
